df2cc2030a6ca8d57eae6a9ba5b4e1ad2e6e225c
Rishi Shah <shahrishi108@gmail.com>
2024-02-25 01:30:33 -0500

    dynamic path, to_address

--- src/commands.rs
+++ src/commands.rs
@@ -11,20 +11,28 @@ use mime_guess::from_path;
 use std::path::Path;
 
 
-pub fn send() {
+pub fn send(to: String, path: String) {
     dotenv().ok();
-    let smtp_username = "shahrishi108@gmail.com";
-    let smtp_password = std::env::var("SMTP_TOKEN").expect("SMTP_TOKEN must be set.").to_string();
-    // let smtp_port = 587;
 
-    let filename = String::from("image.png");
-    let file_path = Path::new("image.png");
-    let filebody = fs::read("image.png").unwrap();
+    let smtp_username = std::env::var("EMAIL").expect("EMAIL must be set, please set this using atm config").to_string();
+    let smtp_password = std::env::var("TOKEN").expect("TOKEN must be set, please set this using atm config").to_string();
+
+    let from_address: Vec<&str> = smtp_username.split('@').collect();
+    let to_address: Vec<&str> = to.split('@').collect();
+
+    let subject = "Here is your file!".to_string();
+    let body = "This is a message from Atmail!".to_string();
+
+    
+
+    let file_path = Path::new(&path);
+    let filename = String::from("file");
+
+    let filebody = fs::read(file_path).unwrap();
     let mime_guess = from_path(file_path).first_or_octet_stream();
     let content_type = ContentType::parse(&mime_guess.to_string()).unwrap();
     let attachment = Attachment::new(filename).body(filebody, content_type);
-
-    
+        
     let from_address = match Address::new("shahrishi108", "gmail.com") {
         Ok(addr) => addr,
         Err(e) => {
@@ -32,7 +40,7 @@ pub fn send() {
         }
     };
 
-    let to_address = match Address::new("space.198tyler", "gmail.com") {
+    let to_address = match Address::new(to_address[0], to_address[1]) {
         Ok(addr) => addr,
         Err(e) => {
             panic!("Failed to create address: {}", e);
@@ -43,13 +51,13 @@ pub fn send() {
     let to_user = Mailbox::new(None, to_address.clone());
 
     let email = Message::builder()
-        .from(from_user)
-        .to(to_user)
-        .subject("hello world!")
+        .from(from_user.clone())
+        .to(to_user.clone())
+        .subject(subject.clone())
         .multipart(
             MultiPart::mixed()
-                .singlepart(SinglePart::html(String::from("Hey, here's your file from Atmail!")))
-                .singlepart(attachment)
+                .singlepart(SinglePart::html(body.clone()))
+                .singlepart(attachment.clone())
         ).unwrap();
 
 
